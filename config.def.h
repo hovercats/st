926041e79d99ebd745a12d6724f27122d69f7ac2
hovercats <hovercatswithlasereyes@protonmail.com>
2023-03-18 18:36:01 +0100

    revert scrollback patch

--- config.def.h
+++ config.def.h
@@ -174,8 +174,6 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 3} },
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 3} },
 	{ XK_NO_MOD,            Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -199,8 +197,6 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ XK_NO_MOD,            XK_Page_Up,     kscrollup,      {.i = 10} },
-	{ XK_NO_MOD,            XK_Page_Down,   kscrolldown,    {.i = 10} },
 };
 
 /*
